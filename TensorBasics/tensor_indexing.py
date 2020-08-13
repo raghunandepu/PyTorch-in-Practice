4a1287537105770ddd2671f9e7221fe820ce03c7
Raghuram Nandepu <raghu.ndp@gmail.com>
2020-08-13 19:58:58 +0530

    Tensor Indexing

--- TensorBasics/tensor_indexing.py
+++ TensorBasics/tensor_indexing.py
@@ -10,3 +10,31 @@ x = torch.rand((batch_size, features))
 
 print(x[0])
 print(x[0].shape) # x[0,:]
+
+print(x[2, 0:10])
+
+x[0, 0] = 100
+print(x)
+indices = [2, 5, 6]
+print(x[indices])
+
+x = torch.rand((3, 5))
+print(x.shape)
+
+rows = torch.tensor([1, 0])
+cols = torch.tensor([4, 0])
+print(x[rows, cols].shape)
+
+
+# More Advanced indexing
+x = torch.arange(10)
+print(x[(x < 2) | (x > 8)])
+print(x[x.remainder(2) == 0])
+
+# Useful operations
+print(torch.where(x > 5, x, x*2))
+print(torch.tensor([0, 0, 0, 2, 2, 3, 4]).unique())
+print(x.ndimension())
+print(x.numel()) # count the number of elements in x
+
+
